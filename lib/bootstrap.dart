92d5fda493ea47cb981be0f3a7f3bddccc41c4fc
sanjeevnode <me.sanjeevks@gmail.com>
2026-02-17 10:52:27 +0530

    Refactor code structure for improved readability and maintainability

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,12 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:secure_note/src/src.dart';
 
 import 'firebase_options.dart';
 
 Future<void> bootstrap() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await setupDependencies();
+  // Initialize Flutter bindings
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  // Preserve the native splash screen until the app is ready
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  // Initialize Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Setup dependencies and initialize Firebase
+  await setupDependencies();
+
+  FlutterNativeSplash.remove();
+
   runApp(const App());
 }
